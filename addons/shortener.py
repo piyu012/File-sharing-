d84c824d2f4010b69adb6f62a288e792f77efb67
piyu012 <pcdelegend@gmail.com>
2025-11-17 12:35:02 +0530

    Update shortener.py

--- addons/shortener.py
+++ addons/shortener.py
@@ -4,7 +4,7 @@ API = os.getenv("SHORTENER_API")
 
 def gen(url):
     try:
-        r = requests.get("https://shrinkme.io/api", params={
+        r = requests.get("https://adrinolinks.com/api", params={
             "api_key": API,
             "format": "json",
             "url": url
