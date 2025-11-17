6057cfa236d99b5a3bfde910361f0430fa8082de
piyu012 <pcdelegend@gmail.com>
2025-11-17 09:03:58 +0530

    Update shortener.py

--- addons/shortener.py
+++ addons/shortener.py
@@ -1,1 +1,14 @@
+import requests, os
 
+API = os.getenv("SHORTENER_API")
+
+def gen(url):
+    try:
+        r = requests.get("https://shrinkme.io/api", params={
+            "api_key": API,
+            "format": "json",
+            "url": url
+        })
+        return r.json().get("shortenedUrl", url)
+    except:
+        return url
