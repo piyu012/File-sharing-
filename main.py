70711e42d416e2ba12ed450758c8c2a17e39a7b0
piyu012 <pcdelegend@gmail.com>
2025-11-17 10:54:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from bot import Bot
-Bot().run()
+import asyncio
+import uvicorn
+from addons.ad_api import bot, api  # FastAPI app + Pyrogram bot
+
+async def start_bot():
+    await bot.start()  # bot background me start
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
+    uvicorn.run(api, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
