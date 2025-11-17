bc8955134881337358bcdecb7d5cf73af69e8a1c
piyu012 <pcdelegend@gmail.com>
2025-11-18 02:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import asyncio
+from bot import Bot
 from ad_api import api
+import uvicorn
+
+async def start_all():
+    bot = Bot()
+    asyncio.create_task(bot.start())  # start Pyrogram bot
+
+    config = uvicorn.Config(api, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    server = uvicorn.Server(config)
+    await server.serve()
+
+if __name__ == "__main__":
+    asyncio.run(start_all())
