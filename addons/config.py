ef86da9fa145ab29463d40f2fc48cbcb310f5ae5
piyu012 <pcdelegend@gmail.com>
2025-11-19 01:10:00 +0530

    Update config.py

--- addons/config.py
+++ addons/config.py
@@ -1,1 +1,16 @@
+import os
 
+HMAC_SECRET = os.getenv("HMAC_SECRET", "secret").encode()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+ADRINO_API = os.getenv("ADRINO_API")
+MONGO_URI = os.getenv("MONGO_URI")
+
+DB_NAME = "filesharebott"
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+HOST = os.getenv("RENDER_EXTERNAL_HOSTNAME", "localhost")
