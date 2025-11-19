14034d5321947836183149dfc6b947417462283c
piyu012 <pcdelegend@gmail.com>
2025-11-19 10:44:43 +0530

    Delete addons directory

--- addons/database/mongo.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from addons.config import MONGO_URI, DB_NAME
-
-mongo = AsyncIOMotorClient(MONGO_URI)
-db = mongo[DB_NAME]
-
-tokens_col = db.tokens
--- addons/utils/utils.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-def encode_payload(text):
-    return base64.urlsafe_b64encode(text.encode()).decode()
-
-def decode_payload(text):
-    return base64.urlsafe_b64decode(text.encode()).decode()
