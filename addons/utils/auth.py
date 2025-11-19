f128be7c1310c79ebe444714bca4aefaa99f6264
piyu012 <pcdelegend@gmail.com>
2025-11-19 07:44:22 +0530

    Update auth.py

--- addons/utils/auth.py
+++ addons/utils/auth.py
@@ -1,5 +1,5 @@
 import hmac, hashlib
-from config import HMAC_SECRET
+from addons.config import HMAC_SECRET
 
 def sign(data: str):
     return hmac.new(HMAC_SECRET, data.encode(), hashlib.sha256).hexdigest()
