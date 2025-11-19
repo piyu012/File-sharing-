92e6a33ed00f4630c9d954e2b5b98932bf85c734
piyu012 <pcdelegend@gmail.com>
2025-11-19 16:01:27 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -2,18 +2,14 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
-from helper_func import encode, get_message_id
-
-from plugins.shortener_shrinkme import shrinkme_shorten # ensure this file exists and env key set [SHRINKME_API_KEY]
+from helper_func import encode, get_message_id, short_url
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
             first_message = await client.ask(
-                text="""Forward The First Message From DB Channel (With Quotes)..
-
-Or Send The DB Channel Post Link""",
+                text="Forward The First Message From DB Channel (With Quotes) or Send DB Channel Post Link",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
                 timeout=60
@@ -25,17 +21,14 @@ Or Send The DB Channel Post Link""",
         if f_msg_id:
             break
         else:
-            await first_message.reply(
-                "❌ Error\n\nThis Forwarded Post Is Not From My DB Channel Or This Link Is Not Taken From DB Channel",
-                quote=True
-            )
+            await first_message.reply("❌ Error
 
+This Forwarded Post is not from DB Channel", quote=True)
+    
     while True:
         try:
             second_message = await client.ask(
-                text="""Forward The Last Message From DB Channel (With Quotes)..
-
-Or Send The DB Channel Post Link""",
+                text="Forward The Last Message From DB Channel (With Quotes) or Send DB Channel Post Link",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
                 timeout=60
@@ -47,36 +40,32 @@ Or Send The DB Channel Post Link""",
         if s_msg_id:
             break
         else:
-            await second_message.reply(
-                "❌ Error\n\nThis Forwarded Post Is Not From My DB Channel Or This Link Is Not Taken From DB Channel",
-                quote=True
-            )
+            await second_message.reply("❌ Error
 
-    # build deep link
+This Forwarded Post is not from DB Channel", quote=True)
+    
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
+    short_link = short_url(link)
+    
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share Link", url=f'https://telegram.me/share/url?url={short_link}')]])
+    
+    await second_message.reply_text(
+        f"<b>✅ Batch Link Generated:</b>
 
-    # SHORTEN HERE
-    try:
-        link = await shrinkme_shorten(link)
-    except Exception:
-        pass
-
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
+{short_link}",
+        quote=True,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
     )
-    await second_message.reply_text(f"<b>Here Is Your Link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
-
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
             channel_message = await client.ask(
-                text="""Forward Message From The DB Channel (With Quotes)..
-
-Or Send The DB Channel Post link""",
+                text="Forward Message From DB Channel (With Quotes) or Send DB Channel Post Link",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
                 timeout=60
@@ -88,21 +77,21 @@ Or Send The DB Channel Post link""",
         if msg_id:
             break
         else:
-            await channel_message.reply(
-                "❌ Error\n\nThis Forwarded Post Is Not From My DB Channel Or This Link Is Not Taken From DB Channel",
-                quote=True
-            )
+            await channel_message.reply("❌ Error
 
+This Forwarded Post is not from DB Channel", quote=True)
+    
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
+    short_link = short_url(link)
+    
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share Link", url=f'https://telegram.me/share/url?url={short_link}')]])
+    
+    await channel_message.reply_text(
+        f"<b>✅ Link Generated:</b>
 
-    # SHORTEN HERE
-    try:
-        link = await shrinkme_shorten(link)
-    except Exception:
-        pass
-
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
+{short_link}",
+        quote=True,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
     )
-    await channel_message.reply_text(f"<b>Here Is Your Link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
