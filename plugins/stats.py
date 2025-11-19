9c56df0a2479584beaf56d9a0a9a911fa167daf0
piyu012 <pcdelegend@gmail.com>
2025-11-19 16:34:08 +0530

    Create stats.py

---
+++ plugins/stats.py
@@ -0,0 +1,15 @@
+from bot import Bot
+from pyrogram.types import Message
+from pyrogram import filters
+from config import ADMINS
+from datetime import datetime
+from helper_func import get_readable_time
+
+@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
+async def stats(bot: Bot, message: Message):
+    now = datetime.now()
+    delta = now - bot.uptime
+    time = get_readable_time(delta.seconds)
+    await message.reply(f"Bot Stats
+
+Uptime: {time}")
