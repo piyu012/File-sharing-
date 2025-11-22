014b6cee63626a10c829255ee249bf8f46e58c93
piyu012 <pcdelegend@gmail.com>
2025-11-22 12:45:13 +0530

    Delete helper_func.py

--- helper_func.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-async def encode(text: str) -> str:
-    return base64.urlsafe_b64encode(text.encode()).decode()
-
-async def decode(text: str) -> str:
-    return base64.urlsafe_b64decode(text.encode()).decode()
